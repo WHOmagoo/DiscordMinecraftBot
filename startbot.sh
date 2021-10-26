917b1fb5df1c399f60b57c8245340ef101639071
whomagoo <hrmagoo@gmail.com>
2021-10-26 09:51:36 -0700

    create a start script that will start the bot in a tmux session

---
+++ startbot.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+PANENAME="discordminecraftbot"
+
+
+cd "$(dirname "$0")" ||  (echo "Couldn't change directory to script location"; exit)
+tmux respawn-pane -k -t $PANENAME
+if [ $? -eq 0 ]
+then
+  echo Restarted bot under existing session named $PANENAME
+else
+  tmux new-session -d -s $PANENAME "python3 MinecraftServerBot.py"
+  if [ $? -eq 0 ]
+  then
+    echo Started bot with new session name $PANENAME
+  else
+    echo Could not start bot with a tmux session
+    exit $?
+  fi
+fi
